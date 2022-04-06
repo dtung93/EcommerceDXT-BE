abdf4813d9565c31613f7a0db1db96196c55545d
localhost\Tung <mrdang825@gmail.com>
2022-04-06 17:20:32 +0700

    #2ndupdate

--- src/main/java/tech/getarrays/apimanager/payload/JwtResponse.java
+++ src/main/java/tech/getarrays/apimanager/payload/JwtResponse.java
@@ -9,6 +9,9 @@ public class JwtResponse {
     private Long id;
     private String username;
     private String email;
+    private String phone;
+    private String avatar;
+    private String address;
 
     public String getRefreshToken() {
         return refreshToken;
@@ -18,13 +21,38 @@ public class JwtResponse {
         this.refreshToken = refreshToken;
     }
 
-    public JwtResponse(String accessToken, String refreshToken, Long id, String username, String email, List<String> roles) {
+    public JwtResponse(String accessToken, String refreshToken, Long id, String username, String email,String avatar, String address,String phone, List<String> roles) {
+
         this.refreshToken=refreshToken;
         this.token = accessToken;
         this.id = id;
         this.username = username;
         this.email = email;
+        this.avatar=avatar;
+        this.address=address;
+        this.phone=phone;
         this.roles = roles;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+    public void setPhone(String phone){
+        this.phone=phone;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+    public void setAvatar(String avatar){
+        this.avatar=avatar;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address){
+        this.address=address;
     }
 
     public String getToken() {
